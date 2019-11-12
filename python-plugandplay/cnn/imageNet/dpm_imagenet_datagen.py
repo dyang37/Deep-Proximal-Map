dac8cfb10384ebc6f50e19a61c5ebd8ceb47dc0e
diyu <yang1467@purdue.edu>
2019-11-12 19:00:09 +0000

    imageNet train

--- python-plugandplay/cnn/imageNet/dpm_imagenet_datagen.py
+++ python-plugandplay/cnn/imageNet/dpm_imagenet_datagen.py
@@ -7,8 +7,11 @@ import pickle
 import copy
 import os
 
-dataset_dir = '/root/datasets/Data/DET/train/'
-dict_name = '/root/datasets/DPM_VGG16_train1000.dat'
+dataset_dir = '/root/datasets/ILSVRC/Data/DET/train/ILSVRC2013_train/'
+dict_name = '/root/datasets/DPM_VGG16_train10_laplace0.1.dat'
+n_cate = 40
+
+dir_pool = ["n01503061","n02108089","n04330267","n04209133","n02802426","n02958343","n02132136","n02280649","n03179701","n03599486"]
 
 def vgg_preprocess(im):
   image = copy.deepcopy(im*255.)
@@ -17,61 +20,59 @@ def vgg_preprocess(im):
   image[:,:,2] -= 123.68
   image = np.expand_dims(image, axis=0)
   return image
-
 vggModel = VGG16(weights="imagenet")
-
 yAv = []
 v = []
 epsil = []
-filepool = []
-for root,_,_ in os.walk(dataset_dir):
-  filepool = np.concatenate([filepool,glob.glob(os.path.join(root,"*.JPEG"))])
-n_samples = np.shape(filepool)[0]
 
-for filename in filepool[0:n_samples//4]:
-  v_k = cv2.resize(cv2.imread(filename), (224, 224)).astype(np.float32)/255.
-  #sig = np.random.uniform(0,0.3)
-  sig = min(abs(np.random.laplace(scale=0.1)), 0.5)
-  epsil_k = np.random.normal(0,sig,v_k.shape)
-  x_k = v_k + epsil_k
-  y = vggModel.predict(vgg_preprocess(x_k))
-  Av = vggModel.predict(vgg_preprocess(v_k))
-  y_Av_k = y-Av
-  yAv.append(y_Av_k)
-  v.append(v_k)
-  epsil.append(epsil_k)
+for subdir in dir_pool:
+  fulldir = os.path.join(dataset_dir,subdir)
+  filepool = glob.glob(os.path.join(fulldir,"*.JPEG"))
+  n_samples = np.shape(filepool)[0]
+  for filename in filepool[0:n_samples//4]:
+    v_k = cv2.resize(cv2.imread(filename), (224, 224)).astype(np.float32)/255.
+    #sig = np.random.uniform(0,0.3)
+    sig = min(abs(np.random.laplace(scale=0.1)), 0.5)
+    epsil_k = np.random.normal(0,sig,v_k.shape)
+    x_k = v_k + epsil_k
+    y = vggModel.predict(vgg_preprocess(x_k))
+    Av = vggModel.predict(vgg_preprocess(v_k))
+    y_Av_k = y-Av
+    yAv.append(y_Av_k)
+    v.append(v_k)
+    epsil.append(epsil_k)
 
-for filename in filepool[n_samples//4:n_samples*2//4]:
-  x_k = cv2.resize(cv2.imread(filename), (224, 224)).astype(np.float32)/255.
-  #sig = np.random.uniform(0,0.3)
-  sig = min(abs(np.random.laplace(scale=0.1)), 0.5)
-  epsil_k = np.random.normal(0,sig,x_k.shape)
-  v_k = x_k - epsil_k
-  y = vggModel.predict(vgg_preprocess(x_k))
-  Av = vggModel.predict(vgg_preprocess(v_k))
-  y_Av_k = y-Av
-  yAv.append(y_Av_k)
-  v.append(v_k)
-  epsil.append(epsil_k)
+  for filename in filepool[n_samples//4:n_samples*2//4]:
+    x_k = cv2.resize(cv2.imread(filename), (224, 224)).astype(np.float32)/255.
+    #sig = np.random.uniform(0,0.3)
+    sig = min(abs(np.random.laplace(scale=0.1)), 0.5)
+    epsil_k = np.random.normal(0,sig,x_k.shape)
+    v_k = x_k - epsil_k
+    y = vggModel.predict(vgg_preprocess(x_k))
+    Av = vggModel.predict(vgg_preprocess(v_k))
+    y_Av_k = y-Av
+    yAv.append(y_Av_k)
+    v.append(v_k)
+    epsil.append(epsil_k)
 
-x_idx_list = np.random.choice(range(n_samples*2//4,n_samples*3//4), size=n_samples//4, replace=False)
-v_idx_list = np.random.choice(range(n_samples*2//4,n_samples*3//4), size=n_samples//4, replace=False)
-for (x_idx,v_idx) in zip(x_idx_list,v_idx_list):
-  x_k = cv2.resize(cv2.imread(filepool[x_idx]), (224, 224)).astype(np.float32)/255.
-  v_k = cv2.resize(cv2.imread(filepool[v_idx]), (224, 224)).astype(np.float32)/255.
-  epsil_k = x_k - v_k
-  y = vggModel.predict(vgg_preprocess(x_k))
-  Av = vggModel.predict(vgg_preprocess(v_k))
-  y_Av_k = y-Av
-  yAv.append(y_Av_k)
-  v.append(v_k)
-  epsil.append(epsil_k)
+  x_idx_list = np.random.choice(range(n_samples*2//4,n_samples*3//4), size=n_samples//4, replace=False)
+  v_idx_list = np.random.choice(range(n_samples*2//4,n_samples*3//4), size=n_samples//4, replace=False)
+  for (x_idx,v_idx) in zip(x_idx_list,v_idx_list):
+    x_k = cv2.resize(cv2.imread(filepool[x_idx]), (224, 224)).astype(np.float32)/255.
+    v_k = cv2.resize(cv2.imread(filepool[v_idx]), (224, 224)).astype(np.float32)/255.
+    epsil_k = x_k - v_k
+    y = vggModel.predict(vgg_preprocess(x_k))
+    Av = vggModel.predict(vgg_preprocess(v_k))
+    y_Av_k = y-Av
+    yAv.append(y_Av_k)
+    v.append(v_k)
+    epsil.append(epsil_k)
 
-for filename in filepool[n_samples*3//4:n_samples]:
-  v_k = cv2.resize(cv2.imread(filename), (224, 224)).astype(np.float32)/255.
-  epsil.append(np.zeros((224,224,3)))
-  yAv.append(np.zeros((1,1000)))
-  v.append(v_k)
+  for filename in filepool[n_samples*3//4:n_samples]:
+    v_k = cv2.resize(cv2.imread(filename), (224, 224)).astype(np.float32)/255.
+    epsil.append(np.zeros((224,224,3)))
+    yAv.append(np.zeros((1,1000)))
+    v.append(v_k)
 
 yAv = np.reshape(yAv,(-1,1000))
 print("shape of v: ",np.shape(v))
--- python-plugandplay/cnn/imageNet/train_imagenet_dpm_vgg16.py
+++ python-plugandplay/cnn/imageNet/train_imagenet_dpm_vgg16.py
@@ -20,8 +20,8 @@ print('training switch: ',_train)
 
 sig = "var"
 sigw = 0.
-model_name = "dpm_model_imageNet/dpm_mixed4_imagenet_vgg16"
-dict_name = "/root/datasets/DPM_VGG16_60000.dat"
+model_name = "dpm_model_imageNet/dpm_imagenet_10cat_laplace0.1"
+dict_name = "/root/datasets/DPM_VGG16_train10_laplace0.1.dat"
 dataset = pickle.load(open(dict_name,"rb"))
 
 y_Av = np.array(dataset['y_Av'])
@@ -35,49 +35,46 @@ print("model name: ",model_name)
 np.random.seed(2019)
 n_train = n_samples
 train_idx = np.random.choice(range(0,n_samples), size=n_train*9//10, replace=False)
-print(train_idx)
 test_idx = list(set(range(0,n_samples))-set(train_idx))
 epsil_train = epsil[train_idx]
 yAv_train = y_Av[train_idx]
 v_train = v[train_idx]
 
-def cnnStack(x,dim=32,ker_size=(3,3),depth=3,upsamp=True):
+
+def cnnStack(x,dim,ker_size=(3,3),depth=3,upsamp=True):
   for _ in range(depth):
     x = layers.Conv2D(dim,ker_size,padding='same',activation='relu')(x)
   if upsamp:
     x = layers.UpSampling2D((2,2))(x)
   return x
 
-############## DPM Model Design
+############## DMP Model Design
 in_shp_y = np.shape(yAv_train)[1:]
 in_shp_v = np.shape(v_train)[1:]
-############## DPM neural network graph
+### construct neural network graph
 input_yAv = layers.Input(shape=(1000,))
 input_v = layers.Input(shape=(rows,cols,channs))
-v_2D = layers.Reshape((224,224,1))(input_v)
-yAv_dense = layers.Dense(1568,activation='relu')(input_yAv)
-yAv_2D = layers.Reshape((7,7,32))(yAv_dense)
+yAv_dense = layers.Dense(49*64,activation='relu')(input_yAv)
+yAv_2D = layers.Reshape((7,7,64))(yAv_dense)
 dim = 64
 for _ in range(5):
   yAv_2D = cnnStack(yAv_2D,dim)
   dim //= 2
-yAv_cnn = layers.Conv2D(1,(3,3),padding = 'same',activation = 'relu')(yAv_2D)
-v_cnn = cnnStack(v_2D,64,depth=2,upsamp=False)
-v_cnn = cnnStack(v_cnn,32,depth=2,upsamp=False)
-v_cnn = cnnStack(v_2D,16,depth=2,upsamp=False)
-v_cnn = layers.Conv2D(1,(3,3),padding = 'same',activation = 'relu')(v_cnn)
-H = layers.concatenate([v_cnn,yAv_cnn])
+yAv_2D = layers.Conv2D(3,(3,3),padding="same",activation="relu")(yAv_2D)
+v_2D = layers.Reshape((224,224,3))(input_v)
+dim = 64
+for _ in range(5):
+  v_2D = cnnStack(v_2D,dim,upsamp=False)
+  dim //= 2
+v_2D = layers.Conv2D(3,(3,3),padding="same",activation="relu")(v_2D)
+H = layers.concatenate([v_2D,yAv_2D])
+H = layers.Conv2D(128,(5,5),padding='same',activation='relu')(H)
 H = layers.Conv2D(64,(5,5),padding='same',activation='relu')(H)
 H = layers.Conv2D(32,(5,5),padding='same',activation='relu')(H)
 H = layers.Conv2D(16,(5,5),padding='same',activation='relu')(H)
 H = layers.Conv2D(8,(5,5),padding='same',activation='relu')(H)
-H = layers.Conv2D(4,(5,5),padding='same',activation='relu')(H)
-H = layers.Conv2D(2,(5,5),padding='same',activation='relu')(H)
-H = layers.Conv2D(1,(5,5),padding='same',activation='tanh')(H)
-H_out = layers.Reshape((224,224))(H)
-
-############## END DPM neural network graph
-
+H = layers.Conv2D(3,(5,5),padding='same',activation='relu')(H)
+H_out = layers.Reshape((224,224,3))(H)
 model = models.Model(inputs=[input_yAv,input_v],output=H_out)
 model = multi_gpu_model(model, gpus=3)
 model.summary()
@@ -85,7 +82,7 @@ model.summary()
 
 
 # Start training
-batch_size = 256
+batch_size = 512
 model.compile(loss='mean_squared_error',optimizer=Adam(lr=0.0005))
 if _train:
   history = model.fit([yAv_train,v_train], epsil_train, epochs=150, batch_size=batch_size,shuffle=True)
