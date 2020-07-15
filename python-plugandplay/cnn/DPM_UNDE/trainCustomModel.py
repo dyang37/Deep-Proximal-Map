8e8a937717dfde95551803bae4a4f58c4e0e154d
Diyu <yang1467@purdue.edu>
2020-07-15 18:13:22 -0400

    trainCustomModel.py

--- python-plugandplay/cnn/DPM_UNDE/trainCustomModel.py
+++ python-plugandplay/cnn/DPM_UNDE/trainCustomModel.py
@@ -7,10 +7,12 @@ import numpy as np
 from keras.utils import multi_gpu_model
 from keras.optimizers import Adam
 from smallModel import cnnModel
+from ptwspdModel import Model19
 ######## model name goes HERE
-model_name = "UNDE_resnet_lossless_1.22_looped3000_gaussian3000"
+model_name = "UNDE_resnet_lossless_1.21_looped3000_gaussian3000-ptwsModel"
 ######## load pre-generated training data
-filepath = '/scratch/gilbreth/yang1467/UNDE/lossless/LosslessDiskdata-sig0.1-2chann.mat'
+'''
+filepath = '/home/yang1467/datasets/UNDE/lossless/LosslessDiskdata-sig0.1-2chann.mat'
 data = h5py.File(filepath, 'r')
 y_Av_train0 = np.transpose(data["yAv_dict"], (2,1,0))
 y_Av_train0 = y_Av_train0[:3000,:,:]
@@ -19,14 +21,19 @@ v_train0 = v_train0[:3000,:,:,:]
 epsil_train0 = np.transpose(data["eps_dict"], (3,0,2,1))
 epsil_train0 = epsil_train0[:3000,:,:,:]
 print("gaussian data: ",np.shape(v_train0))
-filepath = '/scratch/gilbreth/yang1467/UNDE/lossless/LosslessDiskdata-Loopdiff-0.1var-zero-disk-2chann.mat'
+'''
+filepath = '/home/yang1467/datasets/UNDE/lossless/artifact-data-#400.pkl'
+file = open(filepath,"rb")
+data = pickle.load(file)
+y_Av_train0 = np.array(data["yAv"])
+v_train0 = np.array(data["v"])
+epsil_train0 = np.array(data["epsil"])
+print("artifact diff data: ",np.shape(v_train0))
+filepath = '/home/yang1467/datasets/UNDE/lossless/LosslessDiskdata-Loopdiff-0.1var-zero-disk-2chann.mat'
 data = h5py.File(filepath, 'r')
 y_Av_train1 = np.transpose(data["yAv_dict"], (2,1,0))
-y_Av_train1 = y_Av_train1[:3000,:,:]
 v_train1 = np.transpose(data["v_dict"], (3,0,2,1))
-v_train1 = v_train1[:3000,:,:,:]
 epsil_train1 = np.transpose(data["eps_dict"], (3,0,2,1))
-epsil_train1 = epsil_train1[:3000,:,:,:]
 print("looped diff data: ",np.shape(v_train1))
 ######## concatenate all data together
 y_Av_train = np.vstack((y_Av_train0,y_Av_train1))
