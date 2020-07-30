2aeb1f8a1208a33cde06bc355418f376a80f1190
Diyu <yang1467@purdue.edu>
2020-07-30 15:41:11 -0400

    wip

--- python-plugandplay/cnn/DPM_UNDE/ptwspdModel.py
+++ python-plugandplay/cnn/DPM_UNDE/ptwspdModel.py
@@ -73,7 +73,6 @@ def Model18():
   H_out = layers.Lambda(ptwsmultiply,name='prior_ptwspd')([H,lt_yAv])
   H_tanh = layers.Conv2D(2,(1,1),data_format='channels_first',use_bias=False,activation='tanh')(H_out)
   model = models.Model(inputs=[input_yAv,input_v],outputs=H_tanh)
-  model = multi_gpu_model(model, gpus=4)
   return model
 
 def Model19():
@@ -107,5 +106,42 @@ def Model19():
   #### pointwise multiplication
   H_out = layers.Lambda(ptwsmultiply,name='prior_ptwspd')([H_prev,lt_yAv])
   H_tanh = layers.Conv2D(2,(1,1),data_format='channels_first',use_bias=False,activation='tanh')(H_out)
+  model = models.Model(inputs=[input_yAv,input_v],outputs=H_tanh)
+  return model
+
+def Model24():
+  # Construct DPM neural network architecture
+  input_yAv = layers.Input(shape=(10,5200)) # input shape 10x5200
+  input_v = layers.Input(shape=(2,360,260))
+  yAv = residual_stack1D(input_yAv, n_chann=20, downsize=1) # output shape 20x5200
+  yAv = residual_stack1D(yAv, n_chann=40) # output shape 40x2600
+  yAv = residual_stack1D(yAv, n_chann=80) # output shape 80 x 1300
+  yAv = residual_stack1D(yAv, n_chann=360, downsize=5) # output shape 360 x 260
+  yAv2D = layers.Reshape((1,360,260))(yAv)
+  yAv2D = residual_stack2D(yAv2D)
+  yAv2D = residual_stack2D(yAv2D)
+  v = residual_stack2D(input_v)
+  v = residual_stack2D(v)
+  v = residual_stack2D(v)
+  H = layers.Concatenate(axis=1)([v,yAv2D])
+  H = residual_stack2D(H,n_chann=64)
+  H = residual_stack2D(H,n_chann=32)
+  H = residual_stack2D(H,n_chann=16)
+  H = residual_stack2D(H,n_chann=8)
+  H = residual_stack2D(H,n_chann=2)
+  H_prev = layers.Conv2D(2,(1,1),data_format='channels_first',use_bias=False,activation='tanh',name="prev_output")(H)
+  ###### linear transformation to y-A(v)
+  lt_yAv = layers.Conv1D(20,3,strides=1,data_format='channels_first',padding='same',activation=None,use_bias=False,name='lt_layer1')(input_yAv) # 20x5200
+  lt_yAv = layers.Conv1D(40,3,strides=2,data_format='channels_first',padding='same',activation=None,use_bias=False,name='lt_layer2')(lt_yAv) # 40x2600
+  lt_yAv = layers.Conv1D(80,3,strides=2,data_format='channels_first',padding='same',activation=None,use_bias=False,name='lt_layer3')(lt_yAv) # 80x1300
+  lt_yAv = layers.Conv1D(360,3,strides=5,data_format='channels_first',padding='same',activation=None,use_bias=False,name='lt_layer4')(lt_yAv) # 360x260
+  lt_yAv = layers.Reshape((1,360,260))(lt_yAv)
+  lt_yAv = layers.Conv2D(2,(3,3),data_format='channels_first',padding='same',activation=None,use_bias=False,name='lt_layer_output')(lt_yAv) # 2x360x26
+  #### pointwise multiplication
+  H_out = layers.Lambda(ptwsmultiply,name='prior_ptwspd')([H_prev,lt_yAv])
+  H_out = layers.Conv2D(32,(3,3),data_format='channels_first',padding='same',use_bias=False,activation=None)(H_out)
+  H_out = layers.Conv2D(32,(3,3),data_format='channels_first',padding='same',use_bias=False,activation=None)(H_out)
+  H_out = layers.Conv2D(32,(3,3),data_format='channels_first',padding='same',use_bias=False,activation=None)(H_out)
+  H_tanh = layers.Conv2D(2,(3,3),data_format='channels_first',padding='same',use_bias=False,activation='tanh')(H_out)
   model = models.Model(inputs=[input_yAv,input_v],outputs=H_tanh)
   return model
